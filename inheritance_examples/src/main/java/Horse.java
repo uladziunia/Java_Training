fb76c37ef1208d77a7e8bc1b0cd5cf19550ba12f
uladziunia <ulkabar@interia.pl>
2023-07-18 14:58:42 +0200

    All files added.

--- inheritance_examples/src/main/java/Horse.java
+++ inheritance_examples/src/main/java/Horse.java
@@ -1,15 +1,18 @@
-public abstract class Horse implements Animal{
+public abstract class Horse implements Animal {
 
         public void voice() {
+                System.out.println("The Horse voices: iii aaa");
         }
         public void sleep() {
+                System.out.println("The Horse sleeps: Hrrr Hrrr");
         }
         public int move() {
-            return 0;
+                return 10;
         }
         public void eat() {
+                System.out.println("The Horse eats: Am am am");
         }
         public boolean wash() {
-            return true;
+                return true;
         }
 }
