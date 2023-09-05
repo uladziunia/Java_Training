cacf42f1e07a2aefb3f30ff2a46465a831bb0194
uladziunia <ulkabar@interia.pl>
2023-09-05 15:03:40 +0200

    Initial commit

---
+++ BankAccountConsoleApp/src/main/java/org/example/bankaccount/BankAccountConsoleApp.java
@@ -0,0 +1,8 @@
+package org.example.bankaccount;
+
+public class BankAccountConsoleApp {
+    public static void main(String[] args) {
+        System.out.println("Bank Account Console App");
+
+    }
+}
